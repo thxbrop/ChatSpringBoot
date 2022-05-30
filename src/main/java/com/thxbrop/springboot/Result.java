788b1e8ac40d78e6871f2f9e89f0ef26fe1488a2
thxbrop <1365427005@qq.com>
2022-05-31 01:20:54 +0800

    filter changed Add code property for Result.java Add code property for ServerException.java

--- src/main/java/com/thxbrop/springboot/Result.java
+++ src/main/java/com/thxbrop/springboot/Result.java
@@ -2,15 +2,18 @@ package com.thxbrop.springboot;
 
 public class Result<T> {
     private final T data;
+    private final int code;
     private final String message;
 
     public Result(T data) {
         this.data = data;
         this.message = "success";
+        this.code = 200;
     }
 
-    public Result(String message) {
+    public Result(String message, int code) {
         this.message = message;
+        this.code = code;
         this.data = null;
     }
 
@@ -20,6 +23,10 @@ public class Result<T> {
 
     public T getData() {
         return data;
+    }
+
+    public int getCode() {
+        return code;
     }
 
     public boolean success() {
--- src/main/java/com/thxbrop/springboot/ServerException.java
+++ src/main/java/com/thxbrop/springboot/ServerException.java
@@ -1,26 +1,32 @@
 package com.thxbrop.springboot;
 
 public class ServerException extends RuntimeException {
-    public static final ServerException REGISTER_EMAIL_EXIST = new ServerException("Email has been already registered");
-    public static final ServerException LOGIN_EMAIL_EXIST = new ServerException("Email has not been registered");
-    public static final ServerException INVITE_EXIST = new ServerException("You have invited this user");
-    public static final ServerException INVITE_NOT_EXIST = new ServerException("Invite is not existed");
-    public static final ServerException CONVERSATION_NOT_EXIST = new ServerException("Conversation is not existed");
-    public static final ServerException INVITE_APPROVED = new ServerException("Invite has been approved");
-    public static final ServerException INVITE_REFUSED = new ServerException("Invite has been refused");
-    public static final ServerException PERMISSION_DENIED = new ServerException("Permission denied");
-    public static final ServerException MEMBER_EXIST = new ServerException("The user is already a member of this conversation");
-    public static final ServerException USER_NOT_EXIST = new ServerException("The user is not existed");
-    public static final ServerException FROM_USER_NOT_EXIST = new ServerException("The current user is not existed");
-    public static final ServerException TO_USER_NOT_EXIST = new ServerException("The target user is not existed");
-    public static final ServerException WRONG_PASSWORD = new ServerException("The password is incorrect");
-    public static final ServerException INVALIDATE_TOKEN = new ServerException("The token is invalidate");
+    public static final ServerException REGISTER_EMAIL_EXIST = new ServerException("Email has been already registered", 501);
+    public static final ServerException LOGIN_EMAIL_EXIST = new ServerException("Email has not been registered", 502);
+    public static final ServerException INVITE_EXIST = new ServerException("You have invited this user", 503);
+    public static final ServerException INVITE_NOT_EXIST = new ServerException("Invite is not existed", 504);
+    public static final ServerException CONVERSATION_NOT_EXIST = new ServerException("Conversation is not existed", 505);
+    public static final ServerException INVITE_APPROVED = new ServerException("Invite has been approved", 506);
+    public static final ServerException INVITE_REFUSED = new ServerException("Invite has been refused", 507);
+    public static final ServerException PERMISSION_DENIED = new ServerException("Permission denied", 508);
+    public static final ServerException MEMBER_EXIST = new ServerException("The user is already a member of this conversation", 509);
+    public static final ServerException USER_NOT_EXIST = new ServerException("The user is not existed", 510);
+    public static final ServerException FROM_USER_NOT_EXIST = new ServerException("The current user is not existed", 511);
+    public static final ServerException TO_USER_NOT_EXIST = new ServerException("The target user is not existed", 512);
+    public static final ServerException WRONG_PASSWORD = new ServerException("The password is incorrect", 513);
+    public static final ServerException INVALIDATE_TOKEN = new ServerException("The token is invalidate", 514);
 
-    public ServerException(String message) {
+    private final int code;
+
+    public ServerException(String message, int code) {
         super(message);
+        this.code = code;
     }
 
+    public int getCode() {
+        return code;
+    }
     public <T> Result<T> asResult() {
-        return new Result<>(getMessage());
+        return new Result<>(getMessage(), code);
     }
 }
